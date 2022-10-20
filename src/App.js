70ce0ba2254c106888d53f3e52c9683b4cab188a
Anna <momannuta@gmail.com>
2022-10-20 17:36:42 +0300

    rename heroData

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
     <div className="App">
       {
         heroes.map ((hero) =>
-        <HeroCard key = {hero.id}
-        id={hero.id}
+        <HeroCard key = {hero.id} {...hero} 
+        /*id={hero.id}
         img={hero.img}
         name={hero.name}
         universe={hero.universe}
@@ -17,7 +17,8 @@ function App() {
         occupation={hero.occupation}
         friends={hero.friends}
         superpower={hero.superpower}
-        more={hero.more}/>)
+        more={hero.more}*/
+        />)
       }
     </div>
   );
